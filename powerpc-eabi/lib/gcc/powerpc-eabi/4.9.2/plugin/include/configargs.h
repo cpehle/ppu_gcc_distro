131dfc2a92cae475cfd9a0cfc1d8c5a7a37979f4
Christian Pehle <christian.pehle@gmail.com>
2017-11-30 15:50:41 +0100

    Update cross compiler

---
+++ powerpc-eabi/lib/gcc/powerpc-eabi/4.9.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --with-mpfr=/Users/cpehle/Development/ppc-cross/install//include --with-mpc=/Users/cpehle/Development/ppc-cross/install//include --with-gmp=/Users/cpehle/Development/ppc-cross/install//include --prefix=/Users/cpehle/Development/ppc-cross/install/ --target=powerpc-eabi --disable-shared --with-gnu-as --with-as=/Users/cpehle/Development/ppc-cross/install//bin/powerpc-linux-eabi-as --disable-multilib --disable-threads --without-headers --enable-target-optspace --disable-libssp --disable-libquadmath --disable-libgomp --enable-languages=c,c++";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
